1fbc229fcba52f8ad41c55a48d18277bb82403d1
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2021-06-18 20:36:02 +0200

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders uptime monitor', () => {
+test('renders App', () => {
   render(<App />);
-  const element = screen.getByText(/uptime monitor/i);
+  const element = screen.getByText(/Ahmed Hany/i);
   expect(element).toBeInTheDocument();
 });
