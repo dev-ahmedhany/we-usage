bf19074e2a03c3f4344ac9ad6d98d47bab2ec5b6
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2021-06-23 13:33:52 +0200

    show by day

--- src/MultiAxisLine.js
+++ src/MultiAxisLine.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from 'react'
 import { Chart } from 'react-charts'
 import { Resizable } from "re-resizable";
 import { CircularProgress } from '@material-ui/core';
+import Slider from '@material-ui/core/Slider';
 
 export default function Line() {
     // series array
@@ -9,6 +10,8 @@ export default function Line() {
     const [dataSource, setDataSource] = useState();
     const [dataAvg, setDataAvg] = useState();
     const [data, setData] = useState();
+    const [startDate, setStartDate] = React.useState();
+    const [days, setDays] = React.useState(1);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -21,35 +24,49 @@ export default function Line() {
         });
     }, []);
 
-    const getAvgFunction = useCallback((timeInterval, data) => {
+    const getAvgFunction = useCallback((timeInterval, data, startDate) => {
         let lasttime = 0;
-        let finalResult = [];
         let finalResultSpeed = [];
         Object.keys(data).forEach((number, i) => {
-            finalResultSpeed.push({ label: "mb/min", data: [] })
-            finalResult.push({ label: "hany", data: [] })
+            finalResultSpeed.push({ label: "MB/Min", data: [] })
             Object.keys(data[number]).forEach((subscribtion) => {
                 const subInfo = data[number][subscribtion];
-                Object.keys(subInfo).forEach((time, index) => {
+                Object.keys(subInfo).filter((value) => value * timeInterval > startDate).forEach((time, index) => {
 
                     if (index !== 0) {
                         const start = new Date(lasttime * timeInterval);
                         const diff = Math.abs(new Date(time * timeInterval) - start);
-                        const timediff = new Date(start.getTime() + diff / 2)
-                        timediff.setSeconds(0)
-                        const primary = timediff.getTime();
                         const minutes = Math.floor((diff / 1000 / 60));
                         const secondary = (subInfo[time] - subInfo[lasttime]) * 1000 / minutes;
-                        finalResultSpeed[i].data.push({ primary, secondary })
+
+                        // const timediff = new Date(start.getTime() + diff / 2)
+                        // timediff.setSeconds(0)
+                        // const primary = timediff.getTime();
+                        finalResultSpeed[i].data.push({ primary: start.getTime(), secondary })
+                        finalResultSpeed[i].data.push({ primary: (new Date(time * timeInterval)).getTime(), secondary })
                     }
                     lasttime = time
-                    finalResult[i].data.push({ primary: time * timeInterval, secondary: subInfo[time] })
+                })
+            })
+        })
+        return finalResultSpeed;
+    }, []);
+
+    useEffect(() => {
+        if (!dataSource) return;
+        const { timeinterval, data } = dataSource;
+        let finalResult = [];
+        Object.keys(data).forEach((number, i) => {
+            Object.keys(data[number]).forEach((subscribtion, j) => {
+                finalResult.push({ label: 'start date ' + subscribtion, data: [] })
+                const subInfo = data[number][subscribtion];
+                Object.keys(subInfo).forEach((time) => {
+                    finalResult[j].data.push({ primary: time * timeinterval, secondary: subInfo[time] })
                 })
             })
         })
         setData(finalResult)
-        return finalResultSpeed;
-    }, [setData]);
+    }, [dataSource]);
 
     useEffect(() => {
         if (!dataAvg) return;
@@ -60,17 +77,33 @@ export default function Line() {
     useEffect(() => {
         if (!dataSource) return;
         const { timeinterval, data } = dataSource;
-        setDataAvg(getAvgFunction(timeinterval, data));
-    }, [dataSource, getAvgFunction]);
+        setDataAvg(getAvgFunction(timeinterval, data, startDate));
+    }, [dataSource, getAvgFunction, startDate]);
 
     const series = React.useMemo(() => ({ showPoints: false }), [])
     const axes = React.useMemo(() => [{ primary: true, type: 'time', position: 'bottom' },
-    { type: 'linear', position: 'left' }], [])
+        { type: 'linear', position: 'left' }], [])
+
+    useEffect(() => {
+        const date = new Date((new Date()).getTime() - days * 24 * 60 * 60 * 1000)
+        date.setHours(0, 0, 0, 0);
+        setStartDate(date);
+    }, [days])
+
+    const handleChange = (event, newValue) => {
+        setDays(newValue);
+    };
 
     return (
         <>
             {isDataAvalible ?
                 <>
+                    <p>Show data from Last </p>
+                    <Slider min={1} max={84} value={days} onChange={handleChange}
+                        style={{ width: "50vw" }} valueLabelDisplay="auto"
+                        marks={[{ value: 1, label: 'day', }, { value: 7, label: 'week', },
+                        { value: 28, label: 'month', }, { value: 84, label: '3 month', }]} aria-labelledby="continuous-slider" />
+                    <br />
                     <br />
                     <Resizable defaultSize={{ width: "85vw", height: "45vw", }}>
                         <Chart data={dataAvg} series={series} axes={axes} tooltip dark />
